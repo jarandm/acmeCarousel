21f9d99c50fcbcb42f665eebd89ad61897d053c6
jarandm <jarand@kryssklipp.no>
2015-03-19 15:32:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var acmeCarousel = (function(){
+	
+	
 	var dom = {
 		addClass: function(classname, element) {
 			element.classList.add(classname);
@@ -45,24 +47,31 @@ var acmeCarousel = (function(){
 			afterTransition:  function() {}
 		};
 		
-		if(typeof userConfig === "string"){
-			var config = defaultConfig;
-			config.selector = userConfig;
-		}
-		else{
-			var config = object.merge(defaultConfig, userConfig);
-		}
-
-		var carouselElement = document.querySelector(config.selector),
-			carouselChildren = carouselElement.querySelector('.slides').children,
-			carouselCounter = document.querySelectorAll('.carousel'),
+		
+		var config,
+			carouselElement,
+			carouselChildren,
+			carouselCounter,
 			allSlides = [],
-			interval,
 			currentSlide,
 			nextSlide,
-			prevSlide;
+			prevSlide,
+			interval;
 			
 		var setup = function(){
+			
+			//Overwrite default config if user defined
+			if(typeof userConfig === "string"){
+				config = defaultConfig;
+				config.selector = userConfig;
+			}
+			else{
+				config = object.merge(defaultConfig, userConfig);
+			}
+			
+			carouselElement = document.querySelector(config.selector);
+			carouselChildren = carouselElement.querySelector('.slides').children;
+			carouselCounter = document.querySelectorAll('.carousel');
 		
 			//Loop slides and add to Array
 			for(var ii = 0, length = carouselChildren.length; ii < length; ii++){
@@ -77,7 +86,7 @@ var acmeCarousel = (function(){
 
 			dom.addClass('active', currentSlide);
 			dom.addClass('next-slide', nextSlide);
-
+			
 			if(config.wrapAround === true){
 				prevSlide = allSlides[allSlides.length-1];
 				dom.addClass('prev-slide', prevSlide);
@@ -87,60 +96,72 @@ var acmeCarousel = (function(){
 			if(config.autoRotation === true){
 				interval = setInterval(goToNextSlide, config.interval);
 			}
+			
+			eventListeners();
 		};
 
 		//Next slide
 		var goToNextSlide = function(){
 
-			//Update slide status
-			if(nextSlide){
-				dom.removeClass('left', carouselElement);
-				dom.addClass('right', carouselElement);
-
-				if(prevSlide){ dom.removeClass('prev-slide', prevSlide); }
-				dom.removeClass('active', currentSlide);
-				dom.removeClass('next-slide', nextSlide);
-
-				var index = allSlides.indexOf(currentSlide);
-
-				prevSlide = currentSlide;
-				currentSlide = allSlides[index+1];
-				nextSlide = allSlides[index+2];
-
-				if(config.wrapAround === true){
-					if(!currentSlide && !nextSlide){
-						currentSlide = allSlides[0];
-						nextSlide = allSlides[1];
-					}
-					else if(!nextSlide){
-						nextSlide = allSlides[0];
-					}
-				}
-
-				if((config.wrapAround === false && index !== (allSlides.length-1)) || config.wrapAround === true){
-					dom.addClass('animating', carouselElement);
-				}
-
-				if(prevSlide){ dom.addClass('prev-slide', prevSlide); }
-				if(nextSlide){ dom.addClass('next-slide', nextSlide); }
-				dom.addClass('active', currentSlide);
-
-				callback();
+			
+			if(!nextSlide){
+				return false;
 			}
+				
+			//Add class to define animation direction
+			dom.removeClass('left', carouselElement);
+			dom.addClass('right', carouselElement);
+
+			//Remove current classes
+			if(prevSlide){ dom.removeClass('prev-slide', prevSlide); }
+			dom.removeClass('active', currentSlide);
+			dom.removeClass('next-slide', nextSlide);
+
+			var index = allSlides.indexOf(currentSlide);
+			
+			//Update slide status
+			prevSlide = currentSlide;
+			currentSlide = allSlides[index+1];
+			nextSlide = allSlides[index+2];
+
+			if(config.wrapAround === true){
+				if(!currentSlide && !nextSlide){
+					currentSlide = allSlides[0];
+					nextSlide = allSlides[1];
+				}
+				else if(!nextSlide){
+					nextSlide = allSlides[0];
+				}
+			}
+
+			//Add class to wrapper while animating
+			if((config.wrapAround === false && index !== (allSlides.length-1)) || config.wrapAround === true){
+				dom.addClass('animating', carouselElement);
+			}
+			
+			//Add classes
+			if(prevSlide){ dom.addClass('prev-slide', prevSlide); }
+			if(nextSlide){ dom.addClass('next-slide', nextSlide); }
+			dom.addClass('active', currentSlide);
+
+			callback();
 		};
 
 		//Previous slide
 		var goToPrevSlide = function(){
 			if(prevSlide){
+				//Add class to define animation direction
 				dom.removeClass('right', carouselElement);
 				dom.addClass('left', carouselElement);
-
+				
+				//Remove current classes
 				if(nextSlide){ dom.removeClass('next-slide', nextSlide); }
 				dom.removeClass('active', currentSlide);
 				dom.removeClass('prev-slide', prevSlide);
 
 				var index = allSlides.indexOf(currentSlide);
-
+				
+				//Update slide status
 				nextSlide = currentSlide;
 				currentSlide = allSlides[index-1];
 				prevSlide = allSlides[index-2];
@@ -180,12 +201,12 @@ var acmeCarousel = (function(){
 			currentSlide.addEventListener('transitionend', transitionEnd);
 		};
 
-
 		//Next slide on eventlistener
 		var eventNext = function(){
 			if(!dom.hasClass('animating', carouselElement)){
 				goToNextSlide();
 				clearInterval(interval);
+				console.log(this);
 			}
 		};
 
@@ -196,30 +217,7 @@ var acmeCarousel = (function(){
 				clearInterval(interval);
 			}
 		};
-
-		//Click listeneres
-		var prevButton = carouselElement.querySelector('.prev');
-		if(prevButton){ prevButton.addEventListener('click', eventPrev); }
-
-		var nextButton = carouselElement.querySelector('.next');
-		if(nextButton){ nextButton.addEventListener('click', eventNext); }
-
 		
-		//Keyboard listeneres
-		if(carouselCounter.length == 1 && keyboardNav){
-			document.addEventListener('keydown', function(event){
-			
-				switch(event.keyCode){
-					case 37:
-						eventPrev();
-					break;
-					case 39:
-						eventNext();
-					break;
-				}
-			});
-		}
-
 		var touchEvents = {
 			start: {x: 0, y: 0},
 			current: {x: 0, y: 0},
@@ -253,12 +251,37 @@ var acmeCarousel = (function(){
 			}
 		};
 		
-		//Touch listeneres
-		if(config.touchNav){
-			carouselElement.addEventListener('touchstart', touchEvents.touchStart);
-			carouselElement.addEventListener('touchmove', touchEvents.touchMove);
-			carouselElement.addEventListener('touchend', touchEvents.touchEnd);
-		}
+		var eventListeners = function(){
+			//Click listeneres
+			var prevButton = carouselElement.querySelector('.prev');
+			if(prevButton){ prevButton.addEventListener('click', eventPrev.bind(this)); }
+	
+			var nextButton = carouselElement.querySelector('.next');
+			if(nextButton){ nextButton.addEventListener('click', eventNext.bind(this)); }
+	
+			
+			//Keyboard listeneres
+			if(carouselCounter.length == 1 && keyboardNav){
+				document.addEventListener('keydown', function(event){
+				
+					switch(event.keyCode){
+						case 37:
+							eventPrev();
+						break;
+						case 39:
+							eventNext();
+						break;
+					}
+				});
+			}
+			
+			//Touch listeneres
+			if(config.touchNav){
+				carouselElement.addEventListener('touchstart', touchEvents.touchStart);
+				carouselElement.addEventListener('touchmove', touchEvents.touchMove);
+				carouselElement.addEventListener('touchend', touchEvents.touchEnd);
+			}
+		};
 		
 		setup();
 		
